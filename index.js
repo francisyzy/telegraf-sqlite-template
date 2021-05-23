4fa760c66abee731d5c84fc2e093b19451a02583
francisyzy <github@francisyzy.com>
2021-05-23 12:44:29 +0800

    Add logging

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ const bot = new Telegraf(API_TOKEN);
 if (process.env.NODE_ENV === "production") {
   bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
   bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
-  if(LOG_GROUPID){
-      
+  if (LOG_GROUPID) {
+    bot.use((ctx) => {
+      if (ctx.message) {
+        ctx.telegram.sendMessage(
+          process.env.GROUP_ID,
+          `name: ${ctx.message.from.first_name}, username: ${ctx.message.from.username}, text: ${ctx.message.text}`,
+        );
+      }
+    });
   }
 } else {
   bot.use(Telegraf.log());
